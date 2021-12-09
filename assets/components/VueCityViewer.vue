ee32ad2e1010e7db2198da6e27806843c03708e6
Chucrut <AntonioMMartel@gmail.com>
2021-12-09 16:05:50 +0000

    Fixed city search input. It now: reescales, limits whitespaces and adapts its height correctly

---
+++ assets/components/VueCityViewer.vue
@@ -0,0 +1,25 @@
+<template>
+    <article>
+        <div class="container-main">
+            <vue-fading-lights-animation/>
+            <div class="container-ui">
+                <div class="title">Pon una ciudad</div>
+                <vue-text-search/>
+            </div>
+        </div>
+    </article>
+</template>
+
+<script>
+import VueTextSearch from './VueTextSearch.vue'
+import VueFadingLightsAnimation from './VueFadingLightsAnimation.vue'
+    export default {
+        name: 'VueArticleSearch',
+        components: { VueFadingLightsAnimation, VueTextSearch }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- assets/components/VueTextSearch.vue
+++ assets/components/VueTextSearch.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="contenedor-flex">
-        <textarea class="text-input" :rows=textRows  @keyup.enter="submit" v-model="input"></textarea>
+        <textarea id="text-input" maxlength="74" :rows=textRows  @keydown.enter.prevent="submit" v-model="input"></textarea>
         <div class="texto-error" v-if="ciudadNoExiste"> {{errorMessage}} </div>    
     </div>
 </template>
@@ -18,30 +18,52 @@
         },
         methods: {
 
-            textAdjust(event) {
-
+            submit() {
+                console.log(this.input)
             },
 
-            submit() {
+            getInputTextWidth() {
+
+                let inputElement = document.getElementById("text-input");
+                // Canvas para determinar el ancho que ocupa cada letra en el input
+                let canvas = document.createElement("canvas");
+                let context = canvas.getContext("2d");
+                context.font = "1.5rem Helvetica" // inputElement.style.fontSize + inputElement.font;
+                let contextWidth = context.measureText(this.input).width;
+                console.log(contextWidth)
+                return Math.ceil(contextWidth);
                 
-                console.log(this.input)
+            },
+
+            updateInputHeight() {
+                if (this.textRows <= 6) this.textRows = this.input.length / 16 + 1;
+                /* Lee ancho de las letras en pixeles
+                    Inutil porque:
+                        1. Diferente en cada browser
+                        2. Las mayusculas se toman en minusculas
+                let inputElement = document.getElementById("text-input");
+                this.textRows = Math.trunc(this.getInputTextWidth() / (inputElement.clientWidth)) + 1;
+                if(this.input.length == 0){
+                    this.textRows = 1;
+                } */
+            },
+            preventMultipleWhitespaces() {
+                if (this.input.slice(-2) == "  "){
+                    this.input= this.input.slice(0,-1)
+                }
             }
+
         },
-        beforeUpdate () {
-            if(this.input.length % 19 == 0){
-                this.textRows = this.input.length / 19 + 1;
-            }
-            if(this.input.length == 0){
-                this.textRows = 1;
-            }
-            return;
+        updated () {
+            this.updateInputHeight();
+            this.preventMultipleWhitespaces(); // Solo un espacio
         }
     }
 </script>
 
 <style lang="scss" scoped>
 
-    .text-input {
+    #text-input {
         text-align: center;
         font-size: 1.5rem;
         resize: none !important;
