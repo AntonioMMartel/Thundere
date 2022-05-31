e00b2f4d95c75338d7ec4ef6cedf269526936dc6
AntonioMMartel <60984007+AntonioMMartel@users.noreply.github.com>
2022-05-31 19:05:18 +0100

    Made interface undraggable and unselectable

--- assets/js/vue/components/NavButton.vue
+++ assets/js/vue/components/NavButton.vue
@@ -1,25 +1,27 @@
 <template>
-        <router-link :to="href" class="contenedor-nav-item"> {{ label }} </router-link>
+  <router-link :to="href" class="contenedor-nav-item"> {{ label }} </router-link>
 </template>
 
 <script>
-    export default {
-        props: ['label','href','user-role']
-    }
+export default {
+  props: ["label", "href", "user-role"],
+};
 </script>
 
 <style lang="scss" scoped>
+.contenedor-nav-item {
+  padding: 1.25em;
+  text-decoration: none;
+  color: $primary-color;
 
-    .contenedor-nav-item {
-        padding: 1.25em;
-        text-decoration: none;
-        color:$primary-color;
+  &:hover {
+    color: $primary-color-darker;
+  }
 
-        &:hover{
-            color:$primary-color-darker
-        }
-
-    }
-
-    
+  -webkit-user-drag: none;
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+}
 </style>
